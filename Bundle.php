61acedfe87fc0a70fbab04bf51446535f8ae5049
tirnanog06 <matthewfitz@gmail.com>
2010-06-15 11:46:24 -0400

    Add Simple CAS Redirect handling

--- Bundle.php
+++ Bundle.php
@@ -14,14 +14,14 @@ use Bundle\SimpleCASBundle\DependencyInjection\SimpleCASExtension;
  */
 class Bundle extends BaseBundle
 {
-    /**
-     * Customizes the Container instance.
-     *
-     * @param Symfony\Components\DependencyInjection\ContainerInterface $container A ContainerInterface instance
-     * @return Symfony\Components\DependencyInjection\BuilderConfiguration A BuilderConfiguration instance
-     */
-    public function buildContainer(ContainerInterface $container)
-    {
-        Loader::registerExtension(new SimpleCASExtension());
-    }
+	/**
+	 * Customizes the Container instance.
+	 *
+	 * @param Symfony\Components\DependencyInjection\ContainerInterface $container A ContainerInterface instance
+	 * @return Symfony\Components\DependencyInjection\BuilderConfiguration A BuilderConfiguration instance
+	 */
+	public function buildContainer(ContainerInterface $container)
+	{
+		Loader::registerExtension(new SimpleCASExtension());
+	}
 }
--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -6,23 +6,24 @@ use Symfony\Framework\WebBundle\Controller;
 
 class AuthController extends Controller
 {
-    public function loginAction()
-    {
-        $this->container->getCasService()->forceAuthentication();
-        
-        if ($this->container->getParameter('simplecas.login_redirect_route')) {
-            return $this->redirect($this->generateUrl($this->container->getParameter('simplecas.login_redirect_route'), array(), true));
-        } elseif ($this->container->getParameter('simplecas.login_redirect_url')) {
-            return $this->redirect($this->container->getParameter('simplecas.login_redirect_url'));
-        }
-        
-        $parameters = array('username' => $this->container->getCasService()->getUsername());
-        
-        return $this->render('SimpleCASBundle:Auth:login:php', $parameters);
-    }
+	public function loginAction()
+	{
+		$this->container->getCasService()->forceAuthentication();
 
-    public function logoutAction()
-    {
-        $this->container->getCasService()->logout();
-    }
+		if ($this->container->getParameter('simplecas.login_redirect_route')) {
+			return $this->redirect($this->generateUrl($this->container->getParameter('simplecas.login_redirect_route'), array(), true));
+		} elseif ($this->container->getParameter('simplecas.login_redirect_url')) {
+			return $this->redirect($this->container->getParameter('simplecas.login_redirect_url'));
+		}
+
+		$parameters = array('username' => $this->container->getCasService()->getUsername());
+
+		return $this->render('SimpleCASBundle:Auth:login:php', $parameters);
+	}
+
+	public function logoutAction()
+	{
+		$this->container->getCasService()->logout('',FALSE);
+		return $this->render('SimpleCASBundle:Auth:logout');
+	}
 }
