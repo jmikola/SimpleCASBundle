4ea17ba71ccda973ae752fe32fbaa84419df8c01
Jeremy Mikola <jmikola@theopenskyproject.com>
2010-07-01 22:04:25 +0800

    Fixing call to getLoginUrl()

--- SimpleCAS.php
+++ SimpleCAS.php
@@ -92,7 +92,7 @@ class SimpleCAS
      */
     public function validateTicket($ticket)
     {
-        if ($uid = $this->protocol->validateTicket($ticket, self::getURL())) {
+        if ($uid = $this->protocol->validateTicket($ticket, self::getLoginUrl())) {
             $this->authenticate($uid);
             return true;
         } else {
