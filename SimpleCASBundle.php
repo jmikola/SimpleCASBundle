c801e5061df855788c0a42c041020fbbbf1a8297
Bulat Shakirzyanov <mallluhuct@gmail.com>
2010-08-13 11:27:50 -0400

    upgraded DI extension registration

--- SimpleCASBundle.php
+++ SimpleCASBundle.php
@@ -2,26 +2,9 @@
 
 namespace Bundle\SimpleCASBundle;
 
-use Symfony\Components\DependencyInjection\ContainerInterface;
-use Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Components\DependencyInjection\ContainerBuilder;
-use Bundle\SimpleCASBundle\DependencyInjection\SimpleCASExtension;
-
 /**
  * SimpleCAS Bundle.
  *
  * @author Jeremy Mikola <jmikola@gmail.com>
  */
-class SimpleCASBundle extends \Symfony\Framework\Bundle\Bundle
-{
-    /**
-     * Customizes the Container instance.
-     *
-     * @param Symfony\Components\DependencyInjection\ContainerInterface $this->container A ContainerInterface instance
-     * @return Symfony\Components\DependencyInjection\BuilderConfiguration A BuilderConfiguration instance
-     */
-    public function buildContainer()
-    {
-        ContainerBuilder::registerExtension(new SimpleCASExtension());
-    }
-}
+class SimpleCASBundle extends \Symfony\Framework\Bundle\Bundle {}
